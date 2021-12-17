557ccfd5f648ae3f6318048280ecd43ac13e6ed2
Alex <alex.bucknall@gmail.com>
2021-12-17 19:19:58 +0000

    Added fixup for dictionaries
    Change-type: minor
    Signed-off-by: Alex Bucknall <alex.bucknall@gmail.com>

---
+++ scripts/sort.js
@@ -0,0 +1,31 @@
+const fs = require("fs").promises;
+const debug = require('debug')('sort');
+const glob = require("glob");
+
+const getDirectories = function (src, callback) {
+    glob(src + '/**/*.txt', callback);
+};
+
+const sortList = function (list) {
+    const words = list.split('\n');
+    const unique = Array.from(new Set(words))
+    const sorted = unique.sort()
+    return sorted;
+}
+
+getDirectories('data', function (err, res) {
+    res.map(async file => {
+        const sort = await fs.readFile(file, "utf-8")
+            .then(
+                (data) => {
+                    return sortList(data);
+                }
+            )
+            .then(
+                async (sorted) => {
+                    await fs.writeFile(`${file}`, sorted.join("\n"));
+                })
+            .catch((err) => console.error('failed to read file', err));
+        return sort;
+    })
+})
